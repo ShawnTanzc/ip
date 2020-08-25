b480ebfffe91019da4169e9950e44ccc17229127
ShawnTanzc <E0406872@u.nus.edu>
2020-08-25 21:19:44 +0800

    add tracking to List in Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,32 @@
+import java.lang.reflect.Array;
 import java.util.Scanner;
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class    Duke {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        ArrayList<String> taskList = new ArrayList<>();
+        String userRequest;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        String username = greet();
-        request(username);
+        greet();
+        while (true) {
+            userRequest = in.nextLine().toLowerCase();
+            if (userRequest.equals("list")) {
+                list(taskList);
+            } else if (userRequest.equals("bye")) {
+                bye();
+            } else {
+                taskList.add(userRequest);
+                System.out.println("added: " + userRequest);
+            }
+        }
     }
-    public static String greet() {
+    public static void greet() {
         Scanner in = new Scanner(System.in);
         HorizontalLine();
         System.out.println("Hello I'm Duke");
@@ -20,54 +34,28 @@ public class    Duke {
         HorizontalLine();
         String username;
         username = in.nextLine();
+        System.out.println("Hello " + username + "! What can I do for you?");
+        HorizontalLine();
 
-        return username;
 
     }
 
-    public static void request(String username) {
-        Scanner in = new Scanner(System.in);
-        String userRequest;
-        HorizontalLine();
-        while (true) {
-            System.out.println("Hello " + username + "! What can I do for you?");
-            HorizontalLine();
-            userRequest = in.nextLine().toLowerCase();
-            if(userRequest.equals("list")) {
-                list();
-                continue;
-            }
-            else if(userRequest.equals("blah")) {
-                blah();
-                continue;
-            }
-            else if(userRequest.equals("bye")) {
-                bye(username);
-                break;
-            }
 
+    public static void list(ArrayList<String> taskList) {
+        HorizontalLine();
+        int listIndex = 1;
+        for (String task : taskList) {
+            System.out.println(listIndex + ". " + task);
+            listIndex++;
         }
+        HorizontalLine();
     }
 
-    public static void list() {
+    public static void bye(){
         HorizontalLine();
-        System.out.println("list");
+        System.out.println("Bye. Hope to see you again soon!");
         HorizontalLine();
-        return;
-    }
 
-    public static void blah() {
-        HorizontalLine();
-        System.out.println("blah");
-        HorizontalLine();
-        return;
-    }
-
-    public static void bye(String username){
-        HorizontalLine();
-        System.out.println("Bye " + username + ". Hope to see you again soon!");
-        HorizontalLine();
-        return;
     }
 
     public static void HorizontalLine() {
